31dbff539d5eb9a2402771470823a0a45e205f9b
Joao Vitor Vitali Barozzi <vitalivitor@gmail.com>
2023-12-30 20:16:31 -0300

    wip

--- src/Control/Concurrent/STM/TStore.hs
+++ src/Control/Concurrent/STM/TStore.hs
@@ -15,133 +15,132 @@ module Control.Concurrent.STM.TStore
     -- * Utils
     , lookupTStore
     , lookupTStore_
-    , indexTStore
     -- * Reexports
     , module STM
     )
 where
 
 
-import Control.DeepSeq
 import Control.Monad
 import qualified Control.Concurrent.STM as STM
 import Control.Concurrent
 import Data.Word (Word32)
-import qualified Foreign.Store as Foreign (Store(Store),lookupStore,readStore,newStore)
+import qualified Foreign.Store as Store
 import Control.Exception (bracket,catch,SomeException(..))
 import GHC.Conc (STM,unsafeIOToSTM,retry)
-import qualified Control.Concurrent.ReadWriteLock as Lock (RWLock, acquireRead, acquireWrite, releaseRead, releaseWrite, new)
-import Data.IORef (IORef,newIORef,writeIORef,readIORef)
+import qualified Control.Concurrent.ReadWriteLock as Lock
+import qualified Control.Concurrent.STM.TVar as TVar
 import Data.Typeable
 
 
--- | Read-only transactional store based on `Foreign.Store.Store`.
+-- | Read-only transactional store based on `Foreign.Store`.
 -- It is read-only so as to keep the mutable state inside the Store, 
 -- under a lock.
 newtype TStore a 
     = TStore 
-        (Foreign.Store 
+        (Store.Store 
             ( Lock.RWLock
-            , TypeRep
-            , IORef a
+            , TVar.TVar a -- We use an TVar here so we don't have to write to the store.
             ))
 
 
 instance Eq (TStore a) where
     {-# INLINE (==) #-}
-    (==) !(TStore (Foreign.Store n0)) !(TStore (Foreign.Store n1)) = 
+    (==) (TStore (Store.Store n0)) !(TStore (Store.Store n1)) = 
         n0 == n1
 
 
 instance Show (TStore a) where
     {-# INLINE show #-}
-    show !(TStore store) = 
+    show (TStore store) = 
         "TStore ("<>show store<>")"
 
 
-newTStore :: (NFData a, Typeable a) => a -> STM (TStore a)
+newTStore :: a -> STM (TStore a)
 {-# INLINE newTStore #-}
-newTStore !(_deepseq2 -> a) = do
-    maybe retry pure =<< unsafeIOToSTM go
-  where
-    go = catch 
-            (fmap Just newTStoreIO) 
-            (\(SomeException _) -> pure Nothing)
-    newTStoreIO = do
-        !lock  <- Lock.new
-        !ioref <- newIORef a
-        !store <- Foreign.newStore (lock, typeOf a, ioref)
-        pure (TStore store)
+newTStore a = do
+    TStore store <- next
+    tvar  <- TVar.newTVar a
+    lock  <- unsafeIOToSTM Lock.new
+    _____ <- _writeStore store (lock, tvar)
+    pure (TStore store)
 
 
-readTStore :: (Typeable a) => TStore a -> STM a
+readTStore :: TStore a -> STM a
 {-# INLINE readTStore #-}
-readTStore !(TStore store) = do
-    ma <- unsafeIOToSTM do
-        !(lock, typeR, spa) <- Foreign.readStore store
-        bracket 
-            (Lock.acquireRead lock >> pure lock) 
-            (\l -> Lock.releaseRead l >> threadDelay 1)
-            (\_ -> do 
-                a <- (\x -> seq x x) <$> readIORef spa
-                pure (if typeOf a == typeR then Just a else Nothing)
-            )
-    maybe retry pure ma
+readTStore (TStore store) = do
+    (lock, tvar) <- _readStore store
+    foo <- unsafeIOToSTM do
+        lockAcquired <- Lock.tryAcquireRead lock
+        if lockAcquired
+            then pure (Just (lock, tvar))
+            else pure Nothing
+    case foo of
+        Nothing -> retry
+        Just (lock,tvar) -> do
+            a <- TVar.readTVar tvar
+            unsafeIOToSTM (Lock.releaseRead lock) 
+            pure a
 
 
-writeTStore :: (NFData a) => TStore a -> a -> STM ()
+writeTStore :: TStore a -> a -> STM ()
 {-# INLINE writeTStore #-}
-writeTStore !(TStore store) !(_deepseq2 -> a) = do 
-    unsafeIOToSTM do
-        !(lock, _typeR, spa) <- Foreign.readStore store
-        bracket 
-            (Lock.acquireWrite lock >> pure lock) 
-            (\l -> Lock.releaseWrite l >> threadDelay 1)
-            (const (writeIORef spa a))
+writeTStore (TStore store) a = do 
+    (lock, tvar) <- _readStore store
+    lockAcquired <- unsafeIOToSTM (Lock.tryAcquireWrite lock)
+    unless lockAcquired retry
+    TVar.writeTVar tvar a 
+    unsafeIOToSTM (Lock.releaseWrite lock)
 
 
--- | Will retry until if finds a store and a value of the expected type
--- inside it.
-lookupTStore :: (Typeable a) => Word32 -> STM (TStore a)
+-- | Will retry until if finds a store.
+lookupTStore :: Word32 -> STM (TStore a)
 {-# INLINE lookupTStore #-}
 lookupTStore !n = do
     maybe retry pure =<< lookupTStore_ n
 
 
 -- | Returns `Nothing` if it can't find a store for the given index
--- or if there are a type mismatch.
-lookupTStore_ :: (Typeable a) => Word32 -> STM (Maybe (TStore a))
+-- or if there are a type mismatches.
+lookupTStore_ :: Word32 -> STM (Maybe (TStore a))
 {-# INLINE lookupTStore_ #-}
 lookupTStore_ !n = do
-    (unsafeIOToSTM (Foreign.lookupStore n)) >>= \case
+    _lookupStore n >>= \case
         Nothing    -> pure Nothing
-        Just store -> do
-            !(lock, typeR, ref) <- unsafeIOToSTM do Foreign.readStore store
-            () <- unsafeIOToSTM (Lock.acquireWrite lock)
-            xx <- unsafeIOToSTM (readIORef ref)
-            () <- unsafeIOToSTM (Lock.releaseWrite lock)
-            () <- unsafeIOToSTM (threadDelay 1)
-            if typeR /= typeOf xx
-                then pure Nothing
-                else pure (Just (TStore store))
+        Just store -> pure (Just (TStore store))
 
 
--------------------------------------------------------------------------------
--- HELPERS --------------------------------------------------------------------
--------------------------------------------------------------------------------
-
-
--- | Get the corresponding index for the store.
-indexTStore :: TStore a -> Word32
-{-# INLINE indexTStore #-}
-indexTStore !(TStore (Foreign.Store n)) = 
-    n
+next :: STM (TStore a)
+{-# INLINE next #-}
+next = do
+    (lock, counterTVar) <- _readStore (_mkStore _offset)
+    lockAcquired <- unsafeIOToSTM (Lock.tryAcquireRead lock)
+    if not lockAcquired
+        then next
+        else do
+            counter <- TVar.readTVar counterTVar
+            let newCounter = (1 + counter)
+            TVar.writeTVar counterTVar newCounter
+            pure (TStore (_mkStore counter))
+    
 
 
 -------------------------------------------------------------------------------
 -- INTERNALS ------------------------------------------------------------------
 -------------------------------------------------------------------------------
 
-_deepseq2 :: NFData a => a -> a
-{-# INLINE _deepseq2 #-}
-_deepseq2 !a = deepseq a a
+
+
+-- | Index offset for the `Foreign.Store`.
+_offset :: Word32
+{-# INLINE _offset #-}
+_offset = maxBound - 67295
+            
+
+_readStore s = unsafeIOToSTM (Store.readStore s)
+
+_writeStore s a = unsafeIOToSTM (Store.writeStore s a)
+
+_lookupStore n = unsafeIOToSTM (Store.lookupStore n)
+
+_mkStore = Store.Store
