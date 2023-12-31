326a5b650bd96e54e6f427d962cbe01e83480d91
Joao Vitor Vitali Barozzi <vitalivitor@gmail.com>
2023-12-31 19:17:54 -0300

    cleanup

--- src/Control/Concurrent/STM/TStore.hs
+++ src/Control/Concurrent/STM/TStore.hs
@@ -2,15 +2,13 @@
 module Control.Concurrent.STM.TStore
     ( -- * Transactional Store
       TStore
-    -- * Creation
     , newTStore
-    -- * Reading
     , readTStore
-    -- * Writting
     , writeTStore
     -- * Utils
     , unsafeLookupTStore
     , tStoreIndex
+    , tStore
     -- * Reexports
     , module STM
     , Word32
@@ -29,17 +27,18 @@ import qualified Control.Concurrent.STM as STM
 import qualified Control.Concurrent.STM.TVar as TVar
 import qualified Foreign.Store as Store
 
+-- TODO how can we go from here to supporting the nested calls?
 
 -- | Parallel-safe store atop `Foreign.Store`.
-newtype TStore a 
-    = TStore { _runTStore :: Store.Store (TVar.TVar a) }
+newtype TStore a = TStore 
+    { _runTStore :: Store.Store (TVar.TVar a) }
 
 
 instance Eq (TStore a) where
     (==) ts0 ts1 =
         (==) 
-            (_runStore $ _runTStore ts0) 
-            (_runStore $ _runTStore ts1)
+            (_runStore (_runTStore ts0))
+            (_runStore (_runTStore ts1))
 
 
 instance Show (TStore a) where
@@ -55,64 +54,114 @@ newTStore a = do
     pure ts
 
 
+-- | If the Store does not exist it will retry until it does.
 readTStore :: TStore a -> STM a
 readTStore ts = do
-    tvar <- _readStore (_runTStore ts)
-    TVar.readTVar tvar
+    _readStore (_runTStore ts) 
+        -- >>= maybe retry TVar.readTVar
+        >>= maybe undefined TVar.readTVar
 
 
+-- | Creates the TStore before writing to it if the TStore does not exists
+-- already.
 writeTStore :: TStore a -> a -> STM ()
 writeTStore ts a = do 
-    tvar <- _readStore (_runTStore ts)
-    TVar.writeTVar tvar a
-
-
-tStoreIndex :: TStore a -> Word32
-tStoreIndex = 
-    _runStore . _runTStore 
+    _readStore (_runTStore ts) 
+        >>= maybe 
+                (_writeStore (_runTStore ts) =<< TVar.newTVar a)
+                (flip TVar.writeTVar a)
 
 
 -- | Returns `Nothing` if it can't find a store for the given index. 
 -- This function is unsafe in regards to the type of the `TStore`, 
 -- since the store may be found, but it may be for another type.
 unsafeLookupTStore :: Word32 -> STM (Maybe (TStore a))
-unsafeLookupTStore n = do
-    _lookupStore n >>= \case
-        Nothing    -> pure Nothing
-        Just store -> pure (Just (TStore store))
+unsafeLookupTStore =
+    fmap (fmap TStore) . _lookupStore
+
+
+-- | Gather the index of the TStore.
+tStoreIndex :: TStore a -> Word32
+tStoreIndex = 
+    _runStore . _runTStore 
+
+
+-- | Constructs a TStore reference, not the real Store in memory.
+tStore :: Word32 -> TStore a
+tStore = 
+    TStore . Store.Store
 
 
 -------------------------------------------------------------------------------
 -- INTERNALS ------------------------------------------------------------------
 -------------------------------------------------------------------------------
+
+
+_counterStore :: TStore a
+_counterStore = 
+    tStore _offset
+
+
+_substore :: TStore a -> Store.Store a
+_substore = 
+    _mkStore . tStoreIndex
+
+
 -- | Index offset for the `Foreign.Store`.
 _offset :: Word32
 _offset = 
     let magicNumber = 67295 
     in (maxBound - magicNumber)
-_nextCounter = do
-    c <- Unsafe.unsafeIOToSTM do 
-              catch 
-                  (fmap Just (Store.readStore (_mkStore _offset)))
-                  (\(_::SomeException) -> do
-                      tvar <- TVar.newTVarIO (_offset + 10)
-                      Store.writeStore (_mkStore _offset) tvar
-                      Just <$> Store.readStore (_mkStore _offset))
-    case c of
-        Nothing -> retry
-        Just var -> do
+
+
+_next :: STM (TStore a)
+_next = do
+    fmap tStore nextCounter
+  where
+    nextCounter = do
+        maybe retry swapCounter =<< getCounterTVar 
+      where
+        swapCounter var = do
             n <- TVar.readTVar var 
             TVar.writeTVar var (1 + n)
             pure n
-_writeStore s a = Unsafe.unsafeIOToSTM (Store.writeStore s a)
-_readStore s = Unsafe.unsafeIOToSTM (Store.readStore s)
-_lookupStore n = Unsafe.unsafeIOToSTM (Store.lookupStore n)
-_mkStore = Store.Store
-_devel k =
-    let isDevelopment = False
-    in if isDevelopment then k else pure ()
-_runStore (Store.Store n) = n
-_next :: STM (TStore a)
-_next = do
-    counter <- _nextCounter
-    pure (TStore (_mkStore counter))
+        getCounterTVar = do 
+            let readStoreIO = 
+                    fmap Just (Store.readStore (_substore _counterStore))
+            let handleErr SomeException{} = do
+                    tvar <- TVar.newTVarIO (1 + _offset)
+                    ____ <- Store.writeStore (_substore _counterStore) tvar
+                    fmap Just (Store.readStore (_substore _counterStore))
+            Unsafe.unsafeIOToSTM (catch readStoreIO handleErr)
+
+
+_writeStore :: Store.Store a -> a -> STM ()
+_writeStore s a = 
+    Unsafe.unsafeIOToSTM do
+        Store.writeStore s a
+
+
+_readStore :: Store.Store a -> STM (Maybe a)
+_readStore s = 
+    Unsafe.unsafeIOToSTM do
+        catch 
+            (Just <$> Store.readStore s) 
+            (\(e::SomeException) -> putStrLn (show e) >> pure Nothing)
+
+
+_lookupStore :: Word32 -> STM (Maybe (Store.Store a)) 
+_lookupStore n = 
+    Unsafe.unsafeIOToSTM do
+        catch 
+            (Store.lookupStore n) 
+            (\(_::SomeException) -> pure Nothing)
+
+
+_mkStore :: Word32 -> Store.Store a
+_mkStore = 
+    Store.Store
+
+
+_runStore :: Store.Store a -> Word32
+_runStore (Store.Store n) = 
+    n
